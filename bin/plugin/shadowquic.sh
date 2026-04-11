bafd0a97f8de4bbfc8397ea3f1ca1ebbcc2af5c4
Samo1408 <ab.0ss0mm@gmail.com>
2026-04-11 22:30:22 +0300

    Update shadowquic.sh

--- bin/plugin/shadowquic.sh
+++ bin/plugin/shadowquic.sh
@@ -2,6 +2,4 @@
 
 bin/plugin/shadowquic/init.sh &&
   bin/plugin/shadowquic/armeabi-v7a.sh &&
-  bin/plugin/shadowquic/arm64-v8a.sh &&
-  bin/plugin/shadowquic/x86.sh &&
-  bin/plugin/shadowquic/x86_64.sh
+  
